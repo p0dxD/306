b57ef45ce2e28e6e36cfdfae695b44b1207c891c
Jose Rodriguez <jose@Joses-MBP.fios-router.home>
2016-09-19 23:43:15 -0400

    implemented some methods in FIFO queue

--- nachos/util/FIFOQueue.java
+++ nachos/util/FIFOQueue.java
@@ -9,5 +9,45 @@ package nachos.util;
  */
 @SuppressWarnings("serial")
 public class FIFOQueue<T> extends java.util.LinkedList<T> implements Queue<T> {
+    private int capacity; //not sure about this
+    /**
+     * Adds an element to this queue, if it is possible to do so immediately
+     * without violating capacity restrictions.
+     * 
+     * @param e  The element to add.
+     * @return  true if the element was successfully added, false if the element
+     * was not added.
+     */
+    public boolean offer(T e){
+	return (this.size() <= capacity)? (this.add(e)): false;
+    }
     
+    /**
+     * Retrieves, but does not remove, the head of this queue, or returns null
+     * if this queue is empty.
+     * 
+     * @return  The element at the head of the queue, or null if the queue is
+     * empty.
+     */
+    public T peek(){
+	return this.getFirst();
+    }
+    
+    /**
+     * Retrieves and removes the head of this queue, or returns null if this queue is empty.
+     * 
+     * @return  the head of this queue, or null if this queue is empty.
+     */
+    public T poll(){
+	return this.removeFirst();
+    }
+    
+    /**
+     * Test whether this queue is currently empty.
+     * 
+     * @return true if this queue is currently empty.
+     */
+    public boolean isEmpty(){
+	return this.isEmpty();
+    }
 }
