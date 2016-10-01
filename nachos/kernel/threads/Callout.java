abdf1e7636e212df04e9d95a8cd6e6c01315b2c2
Jose Rodriguez <jose@Joses-MacBook-Pro.local>
2016-10-01 16:58:46 -0400

    Callout class create

---
+++ nachos/kernel/threads/Callout.java
@@ -0,0 +1,17 @@
+package nachos.kernel.threads;
+
+public class Callout {
+    /**
+     * Schedule a callout to occur at a specified number of
+     * ticks in the future.
+     *
+     * @param runnable  A Runnable to be invoked when the specified
+     * time arrives.
+     * @param ticksFromNow  The number of ticks in the future at
+     * which the callout is to occur.
+     */
+    public void schedule(Runnable runnable, int ticksFromNow){
+	
+    }
+    
+}
