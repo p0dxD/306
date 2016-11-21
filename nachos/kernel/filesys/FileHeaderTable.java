bbd58640cbb27c6317228cea46ad6f4bf615dfb7
kATHRYN <kathryn.blecher@gmail.com>
2016-11-21 14:27:43 -0500

    added locks and completed methods

--- nachos/kernel/filesys/FileHeaderTable.java
+++ nachos/kernel/filesys/FileHeaderTable.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import nachos.kernel.threads.Lock;
+import nachos.kernel.threads.Semaphore;
 
 public class FileHeaderTable {
     
     
     private HashMap<FileHeader, Lock> lockMap = new HashMap<>();
+    
+    private HashMap<FileHeader, Integer> threadsWaiting = new HashMap<>();
+    
+    private Semaphore openSem = new Semaphore("open file header", 1);
     
 
     /** empty constructor */
@@ -22,21 +27,47 @@ public class FileHeaderTable {
      * return false if the fileheader is already open (in use)
      */
     public void openFileHeader(FileHeader f) {
+	// lock access
+	openSem.P();
+	
 	// is the fileheader already open? if so, get and wait on lock
-	if (lockMap.containsKey(f))
+	if (lockMap.containsKey(f)) {
+	    int numWaiting = threadsWaiting.get(f)+1;
+	    threadsWaiting.replace(f,numWaiting);
 	    lockMap.get(f).acquire();
+	}
 	// otherwise, make a new lock and associate it with this header
 	else {
 	    Lock newLock = new Lock("for fileheader " + f.getDisc());
 	    lockMap.put(f, newLock);
+	    // also, add to hashmap threadsWaiting, with initial value 0
+	    // (no one is waiting yet)
+	    threadsWaiting.put(f, 0);
 	}
 	
+	// release
+	openSem.V();
     }
     
     public void closeFileHeader(FileHeader f) {
-	// is anyone waiting on the lock? if so, leave it open.
 	
+	// lock access
+	openSem.P();
+		
+	// is anyone waiting on the lock? if so, leave it open.
+	if (threadsWaiting.get(f) != 0) {
+	    int numWaiting = threadsWaiting.get(f)-1;
+	    threadsWaiting.replace(f,numWaiting);
+	    lockMap.get(f).release();
+	}
 	// otherwise, close it.
+	else {
+	    lockMap.remove(f);
+	    threadsWaiting.remove(f);
+	}
+	
+	// release
+	openSem.V();
     }
 
 }
